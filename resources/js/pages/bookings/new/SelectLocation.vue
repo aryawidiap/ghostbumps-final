0e49f512366f19bb751f6e918d6dd60349fbe4fb
aryawidiap <aryawidiaputra2002@gmail.com>
2025-06-05 17:55:31 +0700

    add Book ticket pages

--- resources/js/pages/bookings/new/SelectLocation.vue
+++ resources/js/pages/bookings/new/SelectLocation.vue
@@ -1,24 +1,37 @@
 <script setup lang="ts">
-import PlaceholderPattern from '@/components/PlaceholderPattern.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/vue3';
+import { Head, useForm } from '@inertiajs/vue3';
 
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
-        title: 'Dashboard',
-        href: '/dashboard',
+        title: 'My Ticket',
+        href: '/bookings',
+    },
+    {
+        title: 'Ticket Booking',
+        href: '/bookings',
+    },
+    {
+        title: 'Select Location',
+        href: '#',
     },
 ];
 
-defineProps(['bookings']);
+defineProps(['locations']);
+
+const form = useForm({
+    jenisPertandingan: null,
+    jumlahPeserta: null,
+});
+
 import { ref } from 'vue'
 
 const showDropdown = ref(false)
 
 const toggleDropdown = () => {
-  showDropdown.value = !showDropdown.value
+    showDropdown.value = !showDropdown.value
 }
 </script>
 
@@ -35,38 +48,37 @@ const toggleDropdown = () => {
             <div class="bg-black text-white font-['Special_Elite'] w-full min-h-screen">
                 <!-- Main Content -->
                 <div class="w-full px-4 py-10">
-                    <div class="text-sm font-['Oswald'] text-white mb-2">My Tickets / Ticket Booking / Select Location
-                    </div>
                     <div class="text-3xl font-bold mb-2">Ticket Booking</div>
                     <div class="text-xl font-light font-['Oswald'] text-gray-300 mb-6 text-center">
                         Please select the location to be visited.
                     </div>
 
-                    <!-- Experience Card -->
-                    <div class="bg-red-900 rounded-3xl overflow-hidden md:flex shadow-lg">
+                    <!-- Location cards -->
+
+                    <div v-for="location in locations" v-bind:key="location.id"
+                        class="bg-red-900 rounded-3xl overflow-hidden mb-6 md:flex shadow-lg">
                         <div class="md:w-1/2">
                             <img src="house.jpg" alt="Ghostbumps Denpasar" class="w-full h-full object-cover" />
                         </div>
                         <div class="md:w-1/2 p-8 space-y-4">
-                            <h2 class="text-2xl font-bold font-['Special_Elite']">Ghostbumps Denpasar</h2>
-                            <p class="italic text-sm">Jalan Gatot Subroto Timur, No. XXX, Denpasar, Bali</p>
+                            <h2 class="text-2xl font-bold font-['Special_Elite']">{{ location.name }}</h2>
+                            <p class="italic text-sm">{{ location.address }}</p>
                             <p class="text-sm leading-relaxed">
-                                In Denpasar, Bali, an abandoned hotel stands in eerie silence, surrounded by ghostly
-                                legends. Once meant to
-                                be a luxurious retreat, the project was mysteriously abandoned, rumored to be cursed for
-                                disturbing sacred
-                                land. Visitors report shadowy figures, echoing footsteps, and whispers in empty halls.
-                                Some claim to see a
-                                ghostly woman in white wandering the corridors. Locals believe the spirits are restless,
-                                forever trapped
-                                within the ruins.
+                                {{ location.description }}
                             </p>
-                            <button
-                                class="bg-white text-red-700 font-bold px-6 py-2 rounded-full hover:bg-gray-200 hover:text-red-900">
-                                Select
-                            </button>
+                            <!-- TODO: form smhw still post -->
+                            <form
+                                @submit.prevent="form.post(route('customer.new.select-time', location), { onSuccess: () => form.reset() })">
+                                <button
+                                    class="bg-white text-red-700 font-bold px-6 py-2 rounded-full hover:bg-gray-200 hover:text-red-900"
+                                    name="location_id" v-bind:value="location.id">
+                                    Select
+                                </button>
+                            </form>
+
                         </div>
                     </div>
+
                 </div>
             </div>
         </div>
--- resources/js/pages/locations/Index.vue
+++ resources/js/pages/locations/Index.vue
@@ -6,37 +6,40 @@ import { Head } from '@inertiajs/vue3';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
-        title: 'Dashboard',
-        href: '/dashboard',
+        title: 'Location',
+        href: '/location',
     },
 ];
 
-defineProps(['bookings']);
-
+defineProps(['locations']);
 </script>
 
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500&family=Special+Elite&display=swap');
+</style>
+
 <template>
-    <Head title="Booking List" />
+    <Head title="Make booking" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-            <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 dark:border-sidebar-border md:min-h-min">
-                <a :href="route('customer.new.select-location')" class="h-100">
-                    <div>Book new ticket +</div>
-                </a>
-            </div>
-            <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 dark:border-sidebar-border md:min-h-min">
-                <h2>All ticket</h2>
-                <div v-for="booking in bookings" v-bind:key="booking.id" class="bg-red-900 rounded-3xl overflow-hidden md:flex shadow-lg">
+            <div class="bg-black text-white font-['Special_Elite'] w-full min-h-screen">
+                <!-- Main Content -->
+                <div class="w-full px-4 py-10">
+                    <div class="text-3xl font-bold mb-6">Location</div>
+
+                    <!-- Location Card -->
+                     <!-- TODO: adjust margin -->
+                    <div v-for="location in locations" v-bind:key="location.id" class="bg-red-900 rounded-3xl overflow-hidden md:flex shadow-lg">
                         <div class="md:w-1/2">
-                            <!-- TODO: change the src to booking:photo_path -->
+                            <!-- TODO: change the src to location:photo_path -->
                             <img src="/img/table-tennis-1428052_1920.jpg" alt="Ghostbumps Denpasar" class="w-full h-full object-cover" />
                         </div>
                         <div class="md:w-1/2 p-8 space-y-4">
-                            <h2 class="text-2xl font-bold font-['Special_Elite']">{{ booking.name }}</h2>
-                            <p class="italic text-sm">{{ booking.address }}</p>
+                            <h2 class="text-2xl font-bold font-['Special_Elite']">{{ location.name }}</h2>
+                            <p class="italic text-sm">{{ location.address }}</p>
                             <p class="text-sm leading-relaxed">
-                                {{ booking.description }}
+                                {{ location.description }}
                             </p>
                             <button
                                 class="bg-white text-red-700 font-bold px-6 py-2 rounded-full hover:bg-gray-200 hover:text-red-900">
@@ -44,6 +47,7 @@ defineProps(['bookings']);
                             </button>
                         </div>
                     </div>
+                </div>
             </div>
         </div>
     </AppLayout>
