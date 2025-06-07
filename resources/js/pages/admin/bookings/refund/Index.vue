f662ffd770136e857b9ac6438d6f900e927fde10
aryawidiap <aryawidiaputra2002@gmail.com>
2025-06-07 14:03:47 +0700

    add booking and refund index for admin

---
+++ resources/js/pages/admin/bookings/refund/Index.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Dashboard',
+        href: '/dashboard',
+    },
+];
+
+defineProps(['bookings']);
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500&family=Special+Elite&display=swap');
+</style>
+<template>
+
+    <Head title="Make booking" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+            <!-- Put code here -->
+        </div>
+    </AppLayout>
+</template>
